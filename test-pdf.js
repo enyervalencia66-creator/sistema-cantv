0134db132af25ee382586901a5de590fe6f107e5
comacapps-art <comacapps@gmail.com>
2026-08-14 02:02:45 -0400

    Remove test files

--- test-pdf.js
+++
@@ -1,6 +0,0 @@
-const fetch = require('node-fetch');
-const http = require('http');
-const app = require('./server'); // Wait, server.js doesn't export app.
-
-// Let's just make a POST request to localhost:3000/api/casos/pdf with a dummy payload
-// But we need a token. We don't have one.
